2ec3745d7d6db60c23713f8d2cddca5f4d883c28
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2020-03-20 12:13:55 +0000

    Add files via upload

---
+++ plotly stacked barchart  excerise.py
@@ -0,0 +1,65 @@
+#######
+# Objective: Create a stacked bar chart from
+# the file ../data/mocksurvey.csv. Note that questions appear in
+# the index (and should be used for the x-axis), while responses
+# appear as column labels.  Extra Credit: make a horizontal bar chart!
+######
+
+# Perform imports here:
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+# create a DataFrame from the .csv file:
+df = pd.read_csv('mocksurvey.txt')
+print(df.head())
+print(df.columns)
+# create traces using a list comprehension:
+trace1 = go.Bar(
+    x=df['Unnamed: 0'],
+    y=df['Strongly Agree'],
+    name = 'Strongly Agree',
+    marker=dict(color='Gold')
+)
+
+trace2 = go.Bar(
+    x=df['Unnamed: 0'],
+    y=df['Somewhat Agree'],
+    name='Somewhat Agree',
+    marker=dict(color='Grey')
+)
+
+trace3 = go.Bar(
+    x=df['Unnamed: 0'],
+    y=df['Neutral'],
+    name='Neutral',
+    marker=dict(color='Greenyellow')
+)
+trace4 = go.Bar(
+    x=df['Unnamed: 0'],
+    y=df['Somewhat Disagree'],
+    name='Somewhat Disagree',
+    marker=dict(color='Hotpink')
+)
+trace5 = go.Bar(
+    x=df['Unnamed: 0'],
+    y=df['Strongly Disagree'],
+    name='Strongly Disagree',
+    marker=dict(color='Aquamarine')
+)
+
+
+
+# create a layout, remember to set the barmode here
+
+data = [trace1, trace2, trace3, trace4, trace5]
+layout = go.Layout(
+    title='Survey Results'
+)
+
+
+
+# create a fig from data & layout, and plot the fig.
+fig = go.Figure(data=data, layout=layout)
+fig.update_layout(barmode='stack')
+pyo.plot(fig, filename='bar3.html')
